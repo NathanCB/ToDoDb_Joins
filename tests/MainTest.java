23ecb32ac81f1d02b585247888776bab60494385
Nathan Butler <nathanbutler@Nathans-MacBook-Pro.local>
2017-03-31 15:44:46 -0500

    first commit

--- tests/MainTest.java
+++ tests/MainTest.java
@@ -8,6 +8,9 @@ import static org.junit.Assert.*;
 
 public class MainTest {
 
+    public MainTest() throws SQLException {
+    }
+
     public Connection startConnection() throws SQLException {
         Connection conn = DriverManager.getConnection("jdbc:h2:mem:test");
         Main.createTables(conn);
@@ -25,5 +28,31 @@ public class MainTest {
         assertTrue(user.password.equals("March"));
     }
 
+    @Test
+    public void testSelectTodo() throws SQLException {
+        Connection conn = startConnection();
+        Main.insertUser(conn, "Zoe", "March");          // create a user
+        Main.insertTodo(conn, "New Test Todo",1);          // create a Todo for that user
+        ToDoItem toDoItem = Main.selectTodo(conn, 1);
+        conn.close();
+        assertTrue(toDoItem != null);
+    }
 
+    @Test
+    public void testUpdateTodo() throws SQLException {
+        Connection conn = startConnection();
+        Main.insertUser(conn, "Zoe", "March");
+        ToDoItem selectTodoItem = Main.selectTodo(conn, 1);
+        Main.updateToDo(conn, 1);
+        conn.close();
+    }
+
+    @Test
+    public void testDeleteTodo() throws SQLException{
+        Connection conn = startConnection();
+        Main.insertUser(conn, "Zoe", "March");
+        ToDoItem deleteTodoItem = Main.selectTodo(conn, 1);
+        Main.deleteTodo(conn, 1);
+        conn.close();
+    }
 }
